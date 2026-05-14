ac99267431ed0d41b20a5d824c65acab1a153977
Luc Guerraz <55530568+lucguerraz@users.noreply.github.com>
2026-05-14 08:22:56 +0200

    feat: load activies geojson on map

--- app/components/app/Map.vue
+++ app/components/app/Map.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
+import type { Activity } from '~~/types'
+
 const config = useRuntimeConfig()
+
+const { data: activities } = await useFetch<Activity[]>('/api/activities')
 
 const style =
   'https://api.maptiler.com/maps/019e2251-1abd-7d34-b750-9d36b8c36cfd/style.json?key=' + config.public.tiler_api_key
@@ -21,5 +25,14 @@ const paint = {
   <MglMap :map-style="style" :center="center" :zoom="zoom" height="100vh" width="100vw" :attributionControl="false">
     <MglNavigationControl position="top-left" />
     <MglAttributionControl position="bottom-left" :compact="true" />
+
+    <mgl-geo-json-source
+      v-for="activity in activities"
+      :key="activity.stravaid + '_geojson'"
+      :source-id="activity.stravaid + '_geojson'"
+      :data="activity.geojson"
+    >
+      <mgl-line-layer :layer-id="activity.stravaid + '_geojson'" :layout="layout" :paint="paint" />
+    </mgl-geo-json-source>
   </MglMap>
 </template>
