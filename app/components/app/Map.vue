b2122168d86d7b70110ca0a24ce285f3ae774021
Luc Guerraz <55530568+lucguerraz@users.noreply.github.com>
2026-05-15 15:35:40 +0200

    feat: add ants to active activity

--- app/components/app/Map.vue
+++ app/components/app/Map.vue
@@ -2,12 +2,14 @@
 import type { Activity } from '~~/types'
 import type { LngLatBoundsLike } from 'maplibre-gl'
 import { useMap } from '@indoorequal/vue-maplibre-gl'
-import * as turf from '@turf/turf'
+import { bbox } from '@turf/turf'
 
 const config = useRuntimeConfig()
 const bus = useEventBus()
 const route = useRoute()
 const mapRef = useMap()
+
+const antsLayerRef = ref('')
 
 const { data: activities } = await useFetch<Activity[]>('/api/activities')
 
@@ -30,6 +32,9 @@ watch(
   () => route.name,
   (newName, oldName) => {
     if (['index', 'stats'].includes(newName as string)) {
+      if (antsLayerRef.value != '' && mapRef.map?.getLayer(antsLayerRef.value)) {
+        mapRef.map?.removeLayer(antsLayerRef.value)
+      }
       fitBoundsAll()
     }
   }
@@ -74,20 +79,79 @@ const fitBoundsAll = () => {
   fitBoundsMap(allFeatures)
 }
 
+const animateAnts = (frameTime: number, stravaid: number, dashArraySequence: number[][], step: number) => {
+  if (!mapRef.map?.getLayer(stravaid + '_geojson_line_ants')) return
+
+  const newStep = Math.floor((frameTime / 100) % dashArraySequence.length)
+
+  if (step != newStep) {
+    mapRef.map?.setPaintProperty(stravaid + '_geojson_line_ants', 'line-dasharray', dashArraySequence[newStep])
+  }
+  requestAnimationFrame((frameTime) => animateAnts(frameTime, stravaid, dashArraySequence, newStep))
+}
+
+const initAnts = (stravaid: string) => {
+  // https://docs.mapbox.com/mapbox-gl-js/example/animate-ant-path/
+  const dashArraySequence = [
+    [0, 8, 6],
+    [1, 8, 5],
+    [2, 8, 4],
+    [3, 8, 3],
+    [4, 8, 2],
+    [5, 8, 1],
+    [6, 8, 0],
+    [0, 1, 6, 7],
+    [0, 2, 6, 6],
+    [0, 3, 6, 5],
+    [0, 4, 6, 4],
+    [0, 5, 6, 3],
+    [0, 6, 6, 2],
+    [0, 7, 6, 1],
+  ]
+
+  if (antsLayerRef.value != '' && mapRef.map?.getLayer(antsLayerRef.value)) {
+    mapRef.map?.removeLayer(antsLayerRef.value)
+  }
+
+  mapRef.map?.addLayer({
+    id: stravaid + '_geojson_line_ants',
+    type: 'line',
+    source: stravaid + '_geojson',
+    paint: {
+      'line-color': '#ffffff',
+      'line-width': 1,
+      'line-dasharray': dashArraySequence[0],
+    },
+  })
+  requestAnimationFrame((frameTime) => animateAnts(frameTime, parseInt(stravaid), dashArraySequence, 0))
+  antsLayerRef.value = stravaid + '_geojson_line_ants'
+}
+
 const fitBoundsActivity = (stravaid: string) => {
   const activity = activities.value?.find((elem) => elem.stravaid === parseInt(stravaid))
 
   if (!activity) return
 
+  if (mapRef.map?.getSource(stravaid + '_geojson')) {
+    initAnts(stravaid)
+  } else {
+    const antsInterval = setInterval(() => {
+      if (mapRef.map?.getSource(stravaid + '_geojson')) {
+        initAnts(stravaid)
+        clearInterval(antsInterval)
+      }
+    }, 1000)
+  }
+
   fitBoundsMap(activity.geojson)
 }
 
 const fitBoundsMap = (geojson: GeoJSON.GeoJSON) => {
-  const bbox = turf.bbox(geojson)
+  const trackbbox = bbox(geojson)
 
   const bounds = [
-    [bbox[0], bbox[1]], // SW corner
-    [bbox[2], bbox[3]], // NE corner
+    [trackbbox[0], trackbbox[1]], // SW corner
+    [trackbbox[2], trackbbox[3]], // NE corner
   ] as LngLatBoundsLike
 
   mapRef.map?.fitBounds(bounds, {
@@ -122,7 +186,12 @@ const fitBoundsMap = (geojson: GeoJSON.GeoJSON) => {
       :source-id="activity.stravaid + '_geojson'"
       :data="activity.geojson"
     >
-      <mgl-line-layer :layer-id="activity.stravaid + '_geojson'" :layout="layout" :paint="paint" @click="handleClick" />
+      <mgl-line-layer
+        :layer-id="activity.stravaid + '_geojson_line'"
+        :layout="layout"
+        :paint="paint"
+        @click="handleClick"
+      />
     </mgl-geo-json-source>
   </MglMap>
 </template>
