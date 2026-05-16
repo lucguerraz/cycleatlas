9b7b06be8698eea32606ec212bea2ef256fbdf44
Luc Guerraz <55530568+lucguerraz@users.noreply.github.com>
2026-05-16 13:41:43 +0200

    fix: sort activities with newest first

--- server/api/activities/index.get.ts
+++ server/api/activities/index.get.ts
@@ -2,7 +2,7 @@ export default defineEventHandler(async (event) => {
   const { user } = await requireUserSession(event)
 
   try {
-    return await ActivitySchema.find({ athleteid: user.id })
+    return await ActivitySchema.find({ athleteid: user.id }).sort({ start_date: -1 })
   } catch (error) {
     return error
   }
