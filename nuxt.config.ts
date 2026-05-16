ad14fb60be6c8ce6496b9fba5efa40ca4395ee09
Luc Guerraz <55530568+lucguerraz@users.noreply.github.com>
2026-05-16 21:12:57 +0200

    feat: add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,6 +25,33 @@ export default defineNuxtConfig({
     preload: true,
     display: 'swap',
   },
+  app: {
+    head: {
+      title: 'CycleAtlas',
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'icon', sizes: '16x16 32x32 64x64', href: '/favicon.ico' },
+        { rel: 'shortcut icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '64x64', href: '/favicon-64.png' },
+        { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon-96.png' },
+        { rel: 'icon', type: 'image/png', sizes: '128x128', href: '/favicon-128.png' },
+        { rel: 'icon', type: 'image/png', sizes: '192x192', href: '/favicon-192.png' },
+        { rel: 'apple-touch-icon', sizes: '120x120', href: '/favicon-120.png' },
+        { rel: 'apple-touch-icon', sizes: '152x152', href: '/favicon-180.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon-180.png' },
+        { rel: 'icon', type: 'image/png', sizes: '192x192', href: '/favicon-192.png' },
+        { rel: 'icon', type: 'image/png', sizes: '512x512', href: '/favicon-512.png' },
+
+        { rel: 'manifest', href: '/manifest.json' },
+      ],
+      meta: [
+        { name: 'theme-color', content: '#0CBACD' },
+        { name: 'msapplication-TileColor', content: '#ffffff' },
+      ],
+    },
+  },
   modules: [
     '@nuxtjs/google-fonts',
     '@nuxtjs/tailwindcss',
