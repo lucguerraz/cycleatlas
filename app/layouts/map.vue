d1c7c150746c44dadb1bd5b50a49d57cf468155d
Luc Guerraz <55530568+lucguerraz@users.noreply.github.com>
2026-05-18 23:50:12 +0200

    feat: add loading skeleton animation for map

--- app/layouts/map.vue
+++ app/layouts/map.vue
@@ -3,8 +3,20 @@ provideEventBus()
 </script>
 
 <template>
-  <ClientOnly fallback-tag="span" fallback="Loading map..."> <AppMap /> </ClientOnly>
-  <AppProgress />
-  <AppLogo />
-  <slot />
+  <div class="h-screen w-screen bg-gray-100">
+    <div>
+      <ClientOnly>
+        <AppMap />
+        <template #fallback>
+          <span class="sr-only">Loading map...</span>
+        </template>
+      </ClientOnly>
+      <div
+        class="h-screen w-screen overflow-hidden after:relative after:-left-[250vw] after:block after:h-screen after:w-[600vw] after:animate-loaderpulse after:bg-gradient-to-r after:from-gray-100 after:via-[color-mix(in_oklch,theme(colors.gray.100)_80%,black)] after:via-0% after:to-gray-100"
+      ></div>
+    </div>
+    <AppProgress />
+    <AppLogo />
+    <slot />
+  </div>
 </template>
