216de42c16eddd00297830763215ac38cb2e33fe
Luc Guerraz <55530568+lucguerraz@users.noreply.github.com>
2026-05-20 12:19:20 +0200

    fix: stack map loader and map correctly for no scroll

--- app/layouts/map.vue
+++ app/layouts/map.vue
@@ -4,15 +4,17 @@ provideEventBus()
 
 <template>
   <div class="h-screen w-screen bg-gray-100">
-    <div>
-      <ClientOnly>
-        <AppMap />
-        <template #fallback>
-          <span class="sr-only">Loading map...</span>
-        </template>
-      </ClientOnly>
+    <div class="relative z-0 h-screen w-screen">
+      <div class="z-10">
+        <ClientOnly>
+          <AppMap />
+          <template #fallback>
+            <span class="sr-only">Loading map...</span>
+          </template>
+        </ClientOnly>
+      </div>
       <div
-        class="h-screen w-screen overflow-hidden after:relative after:-left-[250vw] after:block after:h-screen after:w-[600vw] after:animate-loaderpulse after:bg-gradient-to-r after:from-gray-100 after:via-[color-mix(in_oklch,theme(colors.gray.100)_80%,black)] after:via-0% after:to-gray-100"
+        class="absolute left-0 top-0 -z-10 h-screen w-screen overflow-hidden after:relative after:-left-[250vw] after:block after:h-screen after:w-[600vw] after:animate-loaderpulse after:bg-gradient-to-r after:from-gray-100 after:via-[color-mix(in_oklch,theme(colors.gray.100)_80%,black)] after:via-0% after:to-gray-100"
       ></div>
     </div>
     <AppProgress />
