5ab02ca547b0e6fb99729ca23b7cc6cf5c54a2f4
Luc Guerraz <55530568+lucguerraz@users.noreply.github.com>
2026-05-20 08:01:52 +0200

    fix: load geofiles from server assets for them to be available after build

--- server/workers/processActivity.ts
+++ server/workers/processActivity.ts
@@ -178,7 +178,8 @@ const build_geojson = async (stravaActivity: any, athlete: Athlete) => {
 }
 
 const compute_regions = async (geojson: any) => {
-  const GEOJSON_COUNTRIES = JSON.parse(await readFile('./server/utils/geofiles/countries.geojson', 'utf8'))
+  const assets = useStorage('assets:server')
+  const GEOJSON_COUNTRIES = JSON.parse(Buffer.from(await assets.getItem('geofiles/countries.geojson')).toString('utf8'))
 
   const countries = computeIntersections(geojson, GEOJSON_COUNTRIES) as Countries
 
@@ -188,7 +189,7 @@ const compute_regions = async (geojson: any) => {
 
     if (['AT', 'CH', 'DE', 'FR', 'IT'].includes(countryISO)) {
       const GEOJSON_REGION = JSON.parse(
-        await readFile(`./server/utils/geofiles/regions_${countryISO.toLowerCase()}.geojson`, 'utf8')
+        Buffer.from(await assets.getItem(`geofiles/regions_${countryISO.toLowerCase()}.geojson`)).toString('utf8')
       )
       country.regions = computeIntersections(geojson, GEOJSON_REGION)
     }
